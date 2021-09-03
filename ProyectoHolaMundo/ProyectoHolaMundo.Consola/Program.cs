a344020e68aa8eb284a49f372f9fb33ef69f8122
micasessa <micasessa@yahoo.com>
2021-09-02 21:26:14 -0300

    Prueba ProyectoHolaMundo

---
+++ ProyectoHolaMundo/ProyectoHolaMundo.Consola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHolaMundo.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Hola Mundo";
+            Console.WriteLine(frase);   
+        }
+    }
+}
