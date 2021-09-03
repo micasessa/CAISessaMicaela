0e1f4f25c5b63fa7890867ab5c3929c4a9c46720
micasessa <micasessa@yahoo.com>
2021-09-02 22:20:51 -0300

    ProyectoHolaMundov2

---
+++ ProyectoHolaMundov2/ProyectoHolaMundov2.Consola/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoHolaMundov2.Liberia;
+
+namespace ProyectoHolaMundov2.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "HolaMundo";
+            Console.WriteLine(frase);
+        }
+    }
+}
